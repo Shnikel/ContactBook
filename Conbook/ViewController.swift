ee5fed8b591b90fc5a80b050d19cde1623e7f394
Nichole <nichole@Nicholes-iMac.local>
2021-06-07 14:14:24 -0700

    Delegate

--- Conbook/ViewController.swift
+++ Conbook/ViewController.swift
@@ -14,21 +14,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var Contacts: UILabel!
     @IBOutlet weak var SearchBar: UISearchBar!
     @IBOutlet weak var ContactImage: UIImageView!
-    let ContactTable = self.ContactTable
-    
+    @IBOutlet weak var ContactCell: UITableViewCell!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        func TVSubview() {
-            addChild(ContactTable)
-            view.addSubview(ContactTable.view)
-            ContactTable.didMove(toParent: self)
-        }
-        ContactImage.image = UIImage(named: ("icon"))
-        ContactImage.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        ContactImage.layer.cornerRadius =  ContactImage.frame.size.height/2
-        ContactImage.clipsToBounds = true
+                ContactImage.image = UIImage(named: ("icon"))
+                ContactImage.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+                ContactImage.layer.cornerRadius =  ContactImage.frame.size.height/2
+                ContactImage.clipsToBounds = true
         
+        ViewController.ContactTable.datasource = self
+        ViewController.ContactTable.delegate = self
+}
+
+    class ContactTable: UITableView {
+        extension TVDelegate: UITableViewDatasource, UITableViewDelegate {
+                func tableView(_ tableView: UITableView, numberOfRowsInsection section: Int) -> Int {
+                return 6
+                }
+    
+                func tableView(_ tableView: UITableView, cellForRowAt: indexPath: IndexPath) -> UITableViewCell {
+        
+                //some code
+        
+                let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell") as!
+                    ContactCell
+                return cell
+                }
+        }
+    }
+}
+    
+                   
+                   
+                   
       /*  ViewController.ContactTable.datasource = self
         ViewController.ContactTable.delegate = self
         let parent = ViewController()
@@ -37,11 +57,10 @@ class ViewController: UIViewController {
         
         
         
-    }
-    class UITableView: UIScrollView{
+    
+   /* class UITableView: UIScrollView{
         func tableView(_ ContactTable: UITableView, numberOfRowsInSection section: Int) -> Int{
             return 6        }
-    }
-    }
+    }*/
 
 
